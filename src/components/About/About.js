e24647cf6ee18db2efa71a74b1c2458c4e6143a8
Gaktines <119362033+Gaktines@users.noreply.github.com>
2024-05-04 09:39:25 -0600

    jsupdates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
       Hi y`all, my name is George and I`m a Full-stack software engineer with a proven track record in Sales and Management experience in Commercial/Industrial construction and the Military. I am able to not only develop customized applications I also bring leadership, and excellent time management skills to the table to help contribute to a team environment and push to meet or beat the schedule. 
 </p>
 <p className="about__text-two">
-I’m a farm boy with a background in the US Army, and construction so I have spent years completing projects as simple as a barn or a fence and as complex as the largest dormitory in Texas. Despite the setbacks or the circumstances, I come in on time, under budget and collaborate with teammates and stakeholders alike while making the margins necessary to make a profit.
+I’m a farm boy form Texas with a background in the US Army, and construction so I have spent years completing projects as simple as a barn or a fence and as complex as the largest dormitory in Texas. Despite the setbacks or the circumstances, I come in on time, under budget and collaborate with teammates and stakeholders alike while making the margins necessary to make a profit.
 </p>
 <p className="about__text-three">
 I enjoy building apps and websites because it’s building– just virtually instead of in person. Coding allows me the ability to use my creative side, with apps like Figma, to build fully functional, flexible App that works across all platforms. 
