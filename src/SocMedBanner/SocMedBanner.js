64e6c1abf32762076f3d6be4e0f3918d2b2ea89d
Gaktines <119362033+Gaktines@users.noreply.github.com>
2024-06-14 09:06:09 -0600

    juspdates

--- src/SocMedBanner/SocMedBanner.js
+++ src/SocMedBanner/SocMedBanner.js
@@ -7,7 +7,7 @@ const SocMedBanner = () => {
       <div id="fb-root"></div>
 <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v19.0" nonce="r6O74qzh"></script>
       <div className="fb-share-button" data-href="https://www.facebook.com/profile.php?id=61556786993898" data-layout="" data-size=""><a className="smBanner__link-facebook" rel="noopener" href="https://www.facebook.com/profile.php?id=61556786993898">Facebook</a></div>
-      <div className="linkedin-share-button" data-href="https://www.linkedin.com/in/george-aktines-b5712151/" data-layout="" data-size=""><a className="smBanner__link-linkedin" rel="noopener" href="https://www.linkedin.com/in/george-aktines-b5712151/">LinkedIn</a></div>
+      <div className="linkedin-share-button" data-href="https://www.linkedin.com/in/gj-a-332749313" data-layout="" data-size=""><a className="smBanner__link-linkedin" rel="noopener" href="https://www.linkedin.com/in/gj-a-332749313">LinkedIn</a></div>
       <div className="github-share-button" data-href="https://github.com/Gaktines" data-layout="" data-size=""><a className="smBanner__link-github" rel="noopener" href="https://github.com/Gaktines">Github</a></div>
 
     </footer>
